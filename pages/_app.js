ad24cd001cd0c8501117216fb8da958fac354fa7
Erik Slatter <erikslatter@gmail.com>
2023-07-06 13:28:35 -0600

    progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '@/styles/globals.css'
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import { UserContext } from '@/lib/context';
+
+import {firestore} from '../lib/firebase';
+import {useUserData} from '../lib/hooks';
 
 function App({ Component, pageProps }) {
+
+  const userData = useUserData();
+
   return (
-  <>
+  <UserContext.Provider value={userData}>
     <Navbar />
     <Component {...pageProps} />
     <Toaster />
-  </>
+  </UserContext.Provider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,43 @@ import styles from '@/styles/Home.module.css'
 import Link from 'next/link';
 import Loader from "../components/Loader";
 import toast from 'react-hot-toast';
+import UserProfile from '../components/UserProflie';
+import PostFeed from '../components/PostFeed';
+import { getUserWithUsername } from '../lib/firebase';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <div>
-      <Loader show />
-      <Link href="/jeffd23" onClick={() => toast.success('Welcome to Jeff!')} >Jeff D 23's Page</Link>
-    </div>
-  )
+// Server-Side Rendering (SSR) for Getting Posts
+export async function getServerSideProps( {query} ) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  //JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+    
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: {user, posts},
+  };
 }
+
+export default function UserProfilePage({user, posts}) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  )
+};
